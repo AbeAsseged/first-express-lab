1529e8c8caecbb136961e394195ec5a81fe54f7d
AbeAsseged <abenezerdaniel@live.com>
2023-08-30 23:22:08 -0500

    first commit

---
+++ server.js
@@ -0,0 +1,8 @@
+const express = require("express")
+
+const app = express();
+
+app.get("/greeting/:name", (request, response) => {
+    response.send(`Hello ${request.params.name}, it's so nice to see you!`);
+});
+
