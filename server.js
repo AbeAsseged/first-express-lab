fbc604fbdd46203709aa10961d0360c41903df2f
AbeAsseged <abenezerdaniel@live.com>
2023-08-30 23:28:35 -0500

    second commit

--- server.js
+++ server.js
@@ -6,3 +6,16 @@ app.get("/greeting/:name", (request, response) => {
     response.send(`Hello ${request.params.name}, it's so nice to see you!`);
 });
 
+//2
+
+app.get("/tip/:total/:tip", (total, tipPercentage) => {
+    tipPercentage.send(`${parseInt(total.params.total) * (parseInt(total.params.tip) / 100)}`)
+})
+
+
+app.listen(3000, function() {
+    console.log("Listening on port 3000")
+})
+
+
+
